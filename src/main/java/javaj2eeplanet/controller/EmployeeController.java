506173f39745038a81c5e7abafc1512aceebee50
RPidugu <reds.j2ee@gmail.com>
2017-02-20 21:00:10 +0100

    Update EmployeeController.java
    changes

--- src/main/java/javaj2eeplanet/controller/EmployeeController.java
+++ src/main/java/javaj2eeplanet/controller/EmployeeController.java
@@ -16,72 +16,76 @@ import javaj2eeplanet.model.EmployeeDao;
 @Controller
 public class EmployeeController {
 
+  
+  
+  @Autowired
+  EmployeeDao employeeDao;
   // ------------------------
   // PUBLIC METHODS
   // ------------------------
   
   /**
-   * /create  --> Create a new user and save it in the database.
+   * /create  --> Create a new employee and save it in the database.
    * 
    * @param email Employee's email
    * @param name Employee's name
-   * @return A string describing if the user is succesfully created or not.
+   * @return A string describing if the employee is succesfully created or not.
    */
   @RequestMapping("/create")
   @ResponseBody
   public String create(String email, String name) {
     Employee user = null;
     try {
-      user = new Employee(email, name);
-      userDao.save(user);
+      employee = new Employee(email, name);
+      employeeDao.save(employee);
     }
     catch (Exception ex) {
-      return "Error creating the user: " + ex.toString();
+      return "Error creating the employee: " + ex.toString();
     }
-    return "Employee succesfully created! (id = " + user.getId() + ")";
+    return "Employee succesfully created! (id = " + employee.getId() + ")";
   }
   
   /**
-   * /delete  --> Delete the user having the passed id.
+   * /delete  --> Delete the employee having the passed id.
    * 
-   * @param id The id of the user to delete
-   * @return A string describing if the user is succesfully deleted or not.
+   * @param id The id of the employee to delete
+   * @return A string describing if the employee is succesfully deleted or not.
    */
   @RequestMapping("/delete")
   @ResponseBody
   public String delete(long id) {
     try {
-      Employee user = new Employee(id);
-      userDao.delete(user);
+      Employee employee = new Employee(id);
+      employeeDao.delete(employee);
     }
     catch (Exception ex) {
-      return "Error deleting the user: " + ex.toString();
+      return "Error deleting the employee: " + ex.toString();
     }
     return "Employee succesfully deleted!";
   }
   
   /**
-   * /get-by-email  --> Return the id for the user having the passed email.
+   * /get-by-email  --> Return the id for the employee having the passed email.
    * 
    * @param email The email to search in the database.
-   * @return The user id or a message error if the user is not found.
+   * @return The employee id or a message error if the user is not found.
    */
   @RequestMapping("/get-by-email")
   @ResponseBody
   public String getByEmail(String email) {
     String userId;
     try {
-      Employee user = userDao.findByEmail(email);
-      userId = String.valueOf(user.getId());
+      Employee employee = employeeDao.findByEmail(email);
+      employeeId = String.valueOf(employee.getId());
     }
     catch (Exception ex) {
       return "Employee not found";
     }
-    return "The user id is: " + userId;
+    return "The employee id is: " + employeeId;
   }
   
   /**
-   * /update  --> Update the email and the name for the user in the database 
+   * /update  --> Update the email and the name for the employee in the database 
    * having the passed id.
    * 
    * @param id The id for the user to update.
@@ -91,24 +95,17 @@ public class EmployeeController {
    */
   @RequestMapping("/update")
   @ResponseBody
-  public String updateUser(long id, String email, String name) {
+  public String updateEmployee(long id, String email, String name) {
     try {
-      Employee user = userDao.findOne(id);
-      user.setEmail(email);
-      user.setName(name);
-      userDao.save(user);
+      Employee employee = employeeDao.findOne(id);
+      employee.setEmail(email);
+      employee.setName(name);
+      employeeDao.save(employee);
     }
     catch (Exception ex) {
-      return "Error updating the user: " + ex.toString();
+      return "Error updating the employee: " + ex.toString();
     }
     return "Employee succesfully updated!";
   }
-
-  // ------------------------
-  // PRIVATE FIELDS
-  // ------------------------
-
-  @Autowired
-  private EmployeeDao userDao;
-  
-} // class EmployeeController
+ 
+} 
